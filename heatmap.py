e2e439cb981111d5526fc03344290bd4f0413f93
winka9587-lab <568151847@qq.com>
2023-07-10 15:35:55 +0800

    fixed bug in get_color_map_feature_pair, now can correctly visualize TSNE result

--- heatmap.py
+++ heatmap.py
@@ -7,29 +7,47 @@ from matplotlib import pyplot as plt
 
 
 def get_color_map(x):
-  colours = plt.cm.Spectral(x)
-  return colours[:, :3]
+    colours = plt.cm.Spectral(x)
+    return colours[:, :3]
 
 def embed_tsne(data):
-  """
-  N x D np.array data
-  """
-  tsne = TSNE(n_components=1, verbose=1, perplexity=40, n_iter=300, random_state=0)
-  tsne_results = tsne.fit_transform(data)
-  tsne_results = np.squeeze(tsne_results)
-  tsne_min = np.min(tsne_results)
-  tsne_max = np.max(tsne_results)
-  return (tsne_results - tsne_min) / (tsne_max - tsne_min)
+    """
+    N x D np.array data
+    """
+    tsne = TSNE(n_components=1, verbose=1, perplexity=40, n_iter=300, random_state=0)
+    tsne_results = tsne.fit_transform(data)
+    tsne_results = np.squeeze(tsne_results)
+    tsne_min = np.min(tsne_results)
+    tsne_max = np.max(tsne_results)
+    return (tsne_results - tsne_min) / (tsne_max - tsne_min)
 
 
 def embed_tsne_pair(data):
-  """
-  N x D np.array data
-  """
-  tsne = TSNE(n_components=1, verbose=1, perplexity=40, n_iter=300, random_state=0)
-  tsne_results = tsne.fit_transform(data)
-  tsne_results_raw = np.squeeze(tsne_results)
-  return tsne_results_raw
+    """
+    N x D np.array data
+    """
+    tsne = TSNE(n_components=1, verbose=1, perplexity=40, n_iter=300, random_state=0)
+    tsne_results = tsne.fit_transform(data)
+    tsne_results_raw = np.squeeze(tsne_results)
+    return tsne_results_raw
+
+
+def embed_tsne_pair_new(data1, data2):
+    """
+    # 生成两组feat在同一尺度下的TSNE color
+    Input:
+        data1: N x D np.array data
+        data2: M x D np.array data
+    Output:
+        tsne_results_raw: (M+N,) np.array data
+        border_idx: 划分两组feat的边界 int
+    """
+    border_idx = data1.shape[0]
+    data_emb = np.vstack((data1, data2))
+    tsne = TSNE(n_components=1, verbose=1, perplexity=40, n_iter=300, random_state=0)
+    tsne_results = tsne.fit_transform(data_emb)
+    tsne_results_raw = np.squeeze(tsne_results)
+    return tsne_results_raw, border_idx
 
 
 def get_color_map_feature(feature):
@@ -41,14 +59,12 @@ def get_color_map_feature(feature):
 # use same normalize parameter
 # feature1, feature2: (N,C)
 def get_color_map_feature_pair(feature1, feature2):
-    tsne_results_raw1 = embed_tsne_pair(feature1)
-    tsne_results_raw2 = embed_tsne_pair(feature2)
-    tsne_min = np.min([np.min(tsne_results_raw1), np.min(tsne_results_raw2)])
-    tsne_max = np.max([np.max(tsne_results_raw1), np.max(tsne_results_raw2)])
-    tsne_results1 = (tsne_results_raw1 - tsne_min) / (tsne_max - tsne_min)
-    tsne_results2 = (tsne_results_raw2 - tsne_min) / (tsne_max - tsne_min)
-    color1 = get_color_map(tsne_results1)
-    color2 = get_color_map(tsne_results2)
+    tsne_results_raw, border_idx = embed_tsne_pair_new(feature1, feature2)
+    tsne_min = np.min(tsne_results_raw)
+    tsne_max = np.max(tsne_results_raw)
+    tsne_results = (tsne_results_raw - tsne_min) / (tsne_max - tsne_min)
+    color1 = get_color_map(tsne_results[:border_idx])
+    color2 = get_color_map(tsne_results[border_idx:])
     return color1, color2
 
 
